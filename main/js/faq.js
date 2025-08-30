27022901279493e7a8000098c252b9c9fed07fc6
aarif753alam <aarif753alam@gmail.com>
2025-08-30 22:22:30 +0530

    Update faq.js

--- main/js/faq.js
+++ main/js/faq.js
@@ -1,1 +1,18 @@
-document.getElementById("faqToggle").addEventListener("click",function(){this.classList.toggle("active"),document.getElementById("faqContent").classList.toggle("active")});const faqItems=document.querySelectorAll('.faq-item-advanced input[type="checkbox"]');faqItems.forEach(e=>{e.addEventListener("change",function(){this.checked&&faqItems.forEach(e=>{e!==this&&(e.checked=!1)})})});
+ // Toggle FAQ section visibility
+        document.getElementById('faqToggle').addEventListener('click', function() {
+            this.classList.toggle('active');
+            document.getElementById('faqContent').classList.toggle('active');
+        });
+        
+        const faqItems = document.querySelectorAll('.faq-item-advanced input[type="checkbox"]');
+        faqItems.forEach(item => {
+            item.addEventListener('change', function() {
+                if (this.checked) {
+                    faqItems.forEach(otherItem => {
+                        if (otherItem !== this) {
+                            otherItem.checked = false;
+                        }
+                    });
+                }
+            });
+        });
